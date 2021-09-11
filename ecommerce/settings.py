e36b340da987175d41ffeb780184e23d008b79da
bhagyajkumar <85674409+bhagyajkumar@users.noreply.github.com>
2021-09-11 18:07:03 +0530

    secret key is now fetched from environment variable

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)jhj)^d(a68y2#$r-d6ksk)h3e82+__q-y2r#g+t#c9ovq&x1'
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "516c8b2c8440491480b68480488bd1c7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
